b0e4f99de2fb4e91388844608eaea28c31aae95a
Clementine <clementine.fouque@gmail.com>
2021-03-03 09:34:12 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# create random users
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "coucou"
+  )
+end
+puts "*" * 20
+puts "Users"
+tp User.all
+
+# create random articles
+35.times do
+  Article.create!(
+    user_id: User.all.sample.id,
+    title: Faker::TvShows::Community.characters,
+    content: Faker::TvShows::Community.quotes
+  )
+end
+puts "*" * 20
+puts "Articles"
+tp Article.all
