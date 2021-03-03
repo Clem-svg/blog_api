f8445b230f6574ec6f6c9bb1a794d9f5425bf716
Clementine <clementine.fouque@gmail.com>
2021-03-03 13:56:22 +0100

    log out working

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_185919) do
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
-    t.datetime "expired_at", null: false
+    t.datetime "exp", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
